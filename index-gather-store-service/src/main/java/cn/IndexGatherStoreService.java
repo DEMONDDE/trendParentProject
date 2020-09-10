dff3aaf638498814c5153f6adc0651c1cf2fdd8f
胡建德 <781140432@qq.com>
2020-09-10 16:51:04 +0800

    添加redis支持

--- index-gather-store-service/src/main/java/cn/IndexGatherStoreService.java
+++ index-gather-store-service/src/main/java/cn/IndexGatherStoreService.java
@@ -2,13 +2,17 @@ package cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author 胡建德
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableCaching
 public class IndexGatherStoreService {
 
     public static void main(String[] args) {
--- index-gather-store-service/src/main/java/cn/service/IndexService.java
+++ index-gather-store-service/src/main/java/cn/service/IndexService.java
@@ -1,7 +1,13 @@
 package cn.service;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.pojo.Index;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheKey;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +19,7 @@ import java.util.Map;
  * @author 胡建德
  */
 @Service
+@CacheConfig(cacheNames = "indices")
 public class IndexService {
 
     @Autowired
@@ -20,6 +27,8 @@ public class IndexService {
 
     private List<Index> indices;
 
+    @HystrixCommand(fallbackMethod = "third_part_not_connected")
+    @Cacheable(key = "'all_codes'")
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> mapList = restTemplate.getForObject("http://127.0.0.1:8090/indexes/codes.json", List.class);
         return mapListToIndex(mapList);
@@ -37,4 +46,13 @@ public class IndexService {
         }
         return indexs;
     }
+
+    public List<Index> third_part_not_connected(){
+        System.out.println("third_part_not_connected()");
+        Index index= new Index();
+        index.setCode("000000");
+        index.setName("无效指数代码");
+        return CollectionUtil.toList(index);
+    }
+
 }
